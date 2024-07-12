4e693c65db212d401be6e15dc19d63886a88e7ab
JLuisRod96 <88519400+Jose96Luis@users.noreply.github.com>
2024-07-11 21:00:54 -0600

    InitialCommit

---
+++ Elektra/Elektra/ElektraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElektraApp.swift
+//  Elektra
+//
+//  Created by José Rodriguez Romero on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ElektraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
